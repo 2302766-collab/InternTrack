80a756944a36d4bd9886be12efdca76918a009e9
2302766-collab <2302766@lnu.edu.ph>
2026-05-17 20:31:04 +0800

    Gender on student

--- laravel/database/seeders/BulkStudentDemoSeeder.php
+++ laravel/database/seeders/BulkStudentDemoSeeder.php
@@ -8,6 +8,7 @@ use App\Models\LogAction;
 use App\Models\LogEntry;
 use App\Models\Role;
 use App\Models\User;
+use App\Services\DashboardCacheService;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
@@ -111,6 +112,7 @@ class BulkStudentDemoSeeder extends Seeder
                             ['email' => $email],
                             [
                                 'name' => "{$firstName} {$lastName}",
+                                'gender' => $faker->randomElement(['Male', 'Female']),
                                 'password' => $defaultPassword,
                                 'role_id' => $studentRoleId,
                             ],
@@ -201,6 +203,8 @@ class BulkStudentDemoSeeder extends Seeder
             ));
             $this->command?->line('Password for all generated students: password');
             $this->command?->line('Password for generated supervisors/advisers: password');
+
+            app(DashboardCacheService::class)->forgetAdminDashboard();
         });
     }
 
